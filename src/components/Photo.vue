523c36432c0be084e7a4486d0459a391392246dc
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-08-04 10:36:37 +0100

    little refactor

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,21 +1,21 @@
 <template>
   <figure class="grid-frigure">
-      <div class="grid-photo-wrap">
-        <img src="@/assets/logo.png" alt="" class="grid-photo">
+    <div class="grid-photo-wrap">
+      <img src="@/assets/logo.png" alt="" class="grid-photo">
+    </div>
+    <figcaption>
+      <p>Caption this</p>
+      <div class="control-buttons">
+        <button class="likes">&hearts; 12</button>
+        <a href="" class="button">
+          <span class="comment-count">
+            <span class="speech-bubble"></span>
+            12
+          </span>
+        </a>
       </div>
-      <figcaption>
-        <p>Caption this</p>
-        <div class="control-buttons">
-          <button class="likes">&hearts; 12</button>
-          <a href="" class="button">
-            <span class="comment-count">
-              <span class="speech-bubble"></span>
-              12
-            </span>
-          </a>
-        </div>
-      </figcaption>
-    </figure>
+    </figcaption>
+  </figure>
 </template>
 
 <script>
