a93a93a0adb02aa28e2f99466c2b0707d48eb202
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-08-04 10:33:53 +0100

    refactored photogrid component

--- src/components/PhotoGrid.vue
+++ src/components/PhotoGrid.vue
@@ -1,29 +1,21 @@
 <template>
   <div class="photo-grid">
-    <figure class="grid-frigure">
-      <div class="grid-photo-wrap">
-        <img src="@/assets/logo.png" alt="" class="grid-photo">
-      </div>
-      <figcaption>
-        <p>Caption this</p>
-        <div class="control-buttons">
-          <button class="likes">&hearts; 12</button>
-          <a href="" class="button">
-            <span class="comment-count">
-              <span class="speech-bubble"></span>
-              12
-            </span>
-          </a>
-        </div>
-      </figcaption>
-    </figure>
+    <Photo />
   </div>
-  
+
 </template>
 
 <script>
+import Photo from './Photo'
 export default {
+  data () {
+    return {
 
+    }
+  },
+  components: {
+    Photo
+  }
 }
 </script>
 
