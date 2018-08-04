99dccd105e23745a00d3902536fd5d66d76559cf
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-08-04 10:24:32 +0100

    setup stylus, refactored code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1>Vuexstagram</h1>
     <router-view/>
   </div>
 </template>
@@ -11,13 +11,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+@require './assets/styles/style.styl'
 </style>
