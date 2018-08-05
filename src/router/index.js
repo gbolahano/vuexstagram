be5f6f409fb99ee02498f3b49cc5029edeea01c2
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-08-05 15:13:04 +0100

    addded route to fetch single posts

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import PhotoGrid from '@/components/PhotoGrid'
-
+import Single from '@/components/Single'
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'PhotoGrid',
       component: PhotoGrid
+    },
+    {
+      path: '/view/:postId',
+      name: 'Single',
+      component: Single
     }
   ]
 })
